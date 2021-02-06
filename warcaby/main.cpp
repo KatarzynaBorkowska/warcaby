468734051982941e6266a409e2e34febd50e131b
Kasia <kasiaboekowska17@gmail.com>
2021-02-06 15:14:21 +0100

    Initial Commit

---
+++ warcaby/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  warcaby
+//
+//  Created by Kasia on 06/02/2021.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
